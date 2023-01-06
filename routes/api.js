c80c677652733575fe7d50a812c1d8cac53207a3
Deiontre10 <travontazlowry@gmail.com>
2023-01-06 18:06:03 -0500

    added post for notes

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,30 @@
 const router = require('express').Router();
-const uuid = require('./helpers/uuid');
-const { readFileSync, readAndAppend } = require('./helpers/file')
+const uuid = require('../helpers/uuid');
+const { readFileSync, readAndAppend } = require('../helpers/file')
 
 router.get('/api/notes', (req, res) => {
     console.info(`${req.method} request received for notes`);
     const data = readFileSync('./db/db.json', 'utf8');
     res.json(JSON.parse(data));
-  });
+});
+
+router.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+  
+    const { title, text } = req.body;
+  
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        id: uuid(),
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+      res.json(`Tip added successfully`);
+    } else {
+      res.error('Error in adding note');
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
 const app = express();
+const apiRoutes = require('./routes/api');
 
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(apiRoutes);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
